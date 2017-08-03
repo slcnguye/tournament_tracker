a2e60f33114be2187dd2c82a42a473717d38b6fa
slcnguye <slcnguye@gmail.com>
2017-08-02 22:49:46 -0400

    Fix image loading from js component.

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -3,6 +3,17 @@ import uiRouter from 'angular-ui-router';
 import routing from './main.routes';
 import _ from 'lodash';
 
+import image1 from '../../assets/images/profile_images/1.png';
+import image2 from '../../assets/images/profile_images/2.jpg';
+import image3 from '../../assets/images/profile_images/3.jpg';
+import image4 from '../../assets/images/profile_images/4.jpg';
+import image5 from '../../assets/images/profile_images/5.jpg';
+import image6 from '../../assets/images/profile_images/6.jpg';
+import image7 from '../../assets/images/profile_images/7.jpg';
+import image8 from '../../assets/images/profile_images/8.jpg';
+import image9 from '../../assets/images/profile_images/9.jpg';
+import image10 from '../../assets/images/profile_images/10.jpg';
+
 export class MainController {
 
   constructor($q, TournamentPlayer, Player, Tournament, Match) {
@@ -70,11 +81,37 @@ export class MainController {
   }
 
   getAvatar(playerId) {
-    return {
-      background: `url(../assets/images/profile_images/${playerId % 10 + 1}.jpg)`,
+    let background;
+    const imageIndex = playerId % 10 + 1;
+    if (imageIndex === 1) {
+      background = image1;
+    } else if (imageIndex === 2) {
+      background = image2;
+    } else if (imageIndex === 3) {
+      background = image3;
+    } else if (imageIndex === 4) {
+      background = image4;
+    } else if (imageIndex === 5) {
+      background = image5;
+    } else if (imageIndex === 6) {
+      background = image6;
+    } else if (imageIndex === 7) {
+      background = image7;
+    } else if (imageIndex === 8) {
+      background = image8;
+    } else if (imageIndex === 9) {
+      background = image9;
+    } else if (imageIndex === 10) {
+      background = image10;
+    }
+
+    const style = {
+      background: `url(${background})`,
       'background-size': 'cover',
       'background-clip': 'content-box'
     };
+
+    return style;
   }
 }
 
